93a9a11981b3a1cb15adea9efa0fd65c2d20ddc7
Victor Alves De Freitas <vitoralves59@gmail.com>
2019-12-10 14:48:23 -0300

    Added routes for chuck norris api

--- Routes/ChuckAPI.js
+++ Routes/ChuckAPI.js
@@ -1,10 +1,17 @@
 const ChuckRoutes = require('express').Router();
+const axios = require('axios').default;
 
 // - GET
 
-ChuckRoutes.get('/jokes/random', (req, res) => {
-    const query = req.query.category
-    res.send({query});
-});
+ChuckRoutes.get('/jokes/random', async (req, res) => {
+    const category = req.query.category
+    const response = await axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`)
+    res.send(JSON.stringify(response.data))
+})
+
+ChuckRoutes.get('/categories', async (req, res) => {
+    const response = await axios.get('https://api.chucknorris.io/jokes/categories')
+    res.send(JSON.stringify(response.data))
+})
 
 module.exports = { ChuckRoutes }
